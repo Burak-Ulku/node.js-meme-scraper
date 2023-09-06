ae4379e5d6a18ccd473b8281e51f54acb7ecc761
Burak Ülkü <brkulku@gmail.com>
2023-09-06 18:06:47 +0200

    Create a meme scraper

---
+++ index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import fs from 'fs';
+
+//const fs = require('fs');
+const target_directory = './memes';
+if (!fs.existsSync(target_directory)) {
+  fs.mkdirSync(target_directory);
+}
+
+axios
+  .get('https://memegen-link-examples-upleveled.netlify.app/')
+  .then(function (res) {
+    // ... do something with the response
+    const html = res.data;
+    console.log(html);
+
+    var re = /<img[^>]+src="?([^"\s]+)"?[^>]*\/>/g;
+    var results = re.exec(html);
+
+    // console.log(results[6]);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
